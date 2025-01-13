19d95997bb95c1a871d56d4484e6f178668fa6e1
AFSAL333 <afsalafs8008@gmail.com>
2025-01-13 18:03:51 +0530

    updated

--- client/src/assets/images/imageLinks.js
+++ client/src/assets/images/imageLinks.js
@@ -1,10 +1,12 @@
 const imageLinks = {
-  auth: 'https://res.cloudinary.com/afsify/image/upload/v1695133216/codisify/auth.jpg',
-  logo: 'https://res.cloudinary.com/afsify/image/upload/v1733133377/codisify/codisify.png',
-  profile: 'https://res.cloudinary.com/afsify/image/upload/v1702630432/codisify/profile.png',
-  vertical: 'https://res.cloudinary.com/afsify/image/upload/v1702631876/codisify/vertical.webp',
-  horizontal: 'https://res.cloudinary.com/afsify/image/upload/v1703225521/codisify/horizontal.png',
+  auth: "https://res.cloudinary.com/afsify/image/upload/v1695133216/codisify/auth.jpg",
+  logo: "https://res.cloudinary.com/afsify/image/upload/v1733133377/codisify/codisify.png",
+  profile:
+    "https://res.cloudinary.com/afsify/image/upload/v1702630432/codisify/profile.png",
+  vertical:
+    "https://res.cloudinary.com/afsify/image/upload/v1702631876/codisify/vertical.webp",
+  horizontal:
+    "https://res.cloudinary.com/afsify/image/upload/v1703225521/codisify/horizontal.png",
 };
-  
+
 export default imageLinks;
-  
